057f8f7a85a18a36133edadf51a45be849f9ac3e
Aqsa Aqeel <aqsa.aqeel17@example.com>
2025-04-23 00:24:50 +0530

    chore: test backend deployment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
+
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -11,7 +13,18 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("🚀 GoGirl Community API is running!");
+});
+
 app.use("/payment", require("./routes/payment"));
 app.use(require("./routes/addMembersInfo"));
+
+// Optional: serve frontend if applicable
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(port, () => console.log(`server started on port ${port}`));
